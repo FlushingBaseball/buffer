1e72e2526b8e26f6ee2991e4739dc602f71108f1
Sam Peck <fullstacksam@protonmail.com>
2023-11-05 23:19:51 -0500

    I understand now

--- script.js
+++ script.js
@@ -1,20 +1,80 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
 const audio = document.getElementById('audio');
-const pause = document.getElementById('pause');
-const play = document.getElementById('play');
-const stop = document.getElementById('stop');
-const currentTime = document.getElementById('current-Time');
-const volume = document.getElementById('volume');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+const songs = ['Cups', 'Sandstorm', 'See You'];
+
+let songIndex = 0;
 
 
-play.addEventListener('click', ()=> audio.play());
-pause.addEventListener('click', ()=> audio.pause());
-stop.addEventListener('click', ()=> {
+//initally load details
+
+function loadSong(song){
+  title.innerText=song;
+  audio.src = `music/${song}.mp3`
+  cover.src = `images/${song}.jpg`
+}
+
+loadSong(songs[songIndex])
+
+function playSong(){
+  musicContainer.classList.add('play')
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+  audio.play();
+}
+function pauseSong(){
+  musicContainer.classList.remove('play')
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
   audio.pause();
-  audio.currentTime=0;
-});
+}
 
-audio.addEventListener('timeupdate', () => {
-  currentTime.innerText = audio.currentTime
+
+playBtn.addEventListener('click', () =>{
+  const isPlaying = musicContainer.classList.contains('play');
+  if (isPlaying){
+    pauseSong();
+  } else {
+    playSong();
+  }
 })
 
-volume.addEventListener('change', () => (audio.volume =  volume.value));
+nextBtn.addEventListener('click', ()=>{
+
+  songIndex++
+  loadSong(songs[songIndex])
+})
+
+prevBtn.addEventListener('click', ()=>{
+  if (songIndex -1 >= 0){
+    songIndex--
+    loadSong(songs[songIndex]) 
+  }
+  else{
+    console.alert("hey")
+  }
+})
+
+// document.querySelector('input[type="file"]').onchange = function(e){
+//   let reader = new FileReader();
+//   reader.onload = function(e) {
+//     let dv = new jDataView(this.result);
+//     //"TAG" starts at byte -128 from EOF.
+//     if (dv.getString(3, dv.byteLength -128)=='TAG'){
+//       let title = dv.getString(30, dv.tell());
+//       let artist = dv.getString(30, dv.tell());
+//       let album = dv.getString(30, dv.tell());
+//       let year = dv.getString(4, dv.tell());
+//     } else {
+//       console.log('No ID3v1 data found')
+//     }
+//   }
+//   reader.readAsArrayBuffer(this.files[0])
+// };
